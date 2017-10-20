9f6603e27068f3fb6ed4a4d31a0fff901126c571
kevinxin90 <kevinxin@scripps.edu>
2017-10-20 09:47:24 -0700

    replace ids with uri

--- src/config.py
+++ src/config.py
@@ -1,60 +1,6 @@
-AVAILABLE_IDS= {"ensembl_gene_id": {
-	"uri": "http://identifiers.org/ensembl.gene/",
-	"example": "ENSG00000139618"
-},
- "omim_id": {
-  "uri": "http://identifiers.org/omim/",
-  "example": 109800
- },
- "entrez_gene_id": {
- 	"uri": "http://identifiers.org/ncbigene/",
- 	"example": 1017
- },
- "hgnc_gene_symbol": {
- 	"uri": "http://identifiers.org/hgnc.symbol/",
- 	"example": "CDK7"
- },
- "hgvs_id": {
- 	"uri": "http://identifiers.org/hgvs/",
- 	"example": "chr6:123456G>A"
- },
- "dbsnp_id": {
- 	"uri": "http://identifiers.org/dbsnp/",
- 	"example": "rs123456"
- },
- "drugbank_id": {
- 	"uri": "http://identifiers.org/drugbank/",
- 	"example": "DB00002"
- },
- "pubchem_id": {
- 	"uri": "http://identifiers.org/pubchem.compound/",
- 	"example": 100101
- },
- "pubmed_id": {
- 	"uri": "http://identifiers.org/pubmed/",
- 	"example": 16333295
- },
- "uniprot_id": {
- 	"uri": "http://identifiers.org/uniprot/",
- 	"example": "P62158"
- },
- "wikipathway_id": {
- 	"uri": "http://identifiers.org/wikipathways/",
- 	"example": "WP100"
- },
- "clinicaltrial_id": {
- 	"uri": "http://identifiers.org/clinicaltrials/",
- 	"example": "NCT01314001"
- },
- "inchikey": {
-  "uri": "http://identifiers.org/inchikey/",
-  "example": "RYYVLZVUVIJVGH-UHFFFAOYSA-N"
- }
-}
-
 AVAILABLE_API_SOURCES={"mygene.info": {
- 	"annotate_ids": ["entrez_gene_id"],
- 	"query_ids": ["uniprot_id", "ensembl_gene_id", "hgnc_gene_symbol", "wikipathway_id", "pubmed_id"],
+ 	"annotate_ids": ["http://identifiers.org/ncbigene/"],
+ 	"query_ids": ["http://identifiers.org/uniprot/", "http://identifiers.org/ensembl.gene/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/wikipathways/", "http://identifiers.org/pubmed/"],
  	"annotate_syntax": "http://mygene.info/v3/gene/{{input}}?dotfield=true",
  	"query_syntax": "http://mygene.info/v3/query?q={{input}}&fetch_all=TRUE&fields=_id",
   "description": "gene annotation service",
@@ -63,8 +9,8 @@ AVAILABLE_API_SOURCES={"mygene.info": {
  	}
  },
   "myvariant.info": {
-  	"annotate_ids": ["hgvs_id"],
-  	"query_ids": ["entrez_gene_id", "hgnc_gene_symbol", "ensembl_gene_id", "dbsnp_id", "pubmed_id", "uniprot_id", "omim_id"],
+  	"annotate_ids": ["http://identifiers.org/hgvs/"],
+  	"query_ids": ["http://identifiers.org/ncbigene/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/ensembl.gene/", "http://identifiers.org/dbsnp/", "http://identifiers.org/pubmed/", "http://identifiers.org/uniprot/", "http://identifiers.org/omim/"],
   	"annotate_syntax": "http://myvariant.info/v1/variant/{{input}}?dotfield=true",
   	"query_syntax": "http://myvariant.info/v1/query?q={{input}}&fetch_all=TRUE&fields=_id",
   	"jsonld": {
@@ -72,8 +18,8 @@ AVAILABLE_API_SOURCES={"mygene.info": {
   	}
   },
   "mychem.info": {
-  	"annotate_ids": ["drugbank_id"],
-  	"query_ids": ["dbsnp_id", "pubchem_id", "drugbank_id", "pubmed_id", "hgnc_gene_symbol", "uniprot_id", "clinicaltrial_id"],
+  	"annotate_ids": ["http://identifiers.org/drugbank/"],
+  	"query_ids": ["http://identifiers.org/dbsnp/", "http://identifiers.org/pubchem.compound/", "http://identifiers.org/drugbank/", "http://identifiers.org/pubmed/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/uniprot/", "http://identifiers.org/clinicaltrials/"],
   	"annotate_syntax": "http://c.biothings.io/v1/drug/{{input}}?dotfield=true",
   	"query_syntax": "http://c.biothings.io/v1/query?q={{input}}&fetch_all=TRUE&fields=_id",
   	"jsonld": {
@@ -85,48 +31,48 @@ AVAILABLE_API_SOURCES={"mygene.info": {
 AVAILABLE_API_ENDPOINTS = [
   {
     "url_syntax": "http://mygene.info/v3/query?q={{input}}&fetch_all=TRUE&fields=_id",
-    "input": ["uniprot_id", "ensembl_gene_id", "hgnc_gene_symbol", "wikipathway_id", "pubmed_id"],
-    "output": ["entrez_gene_id"],
+    "input": ["http://identifiers.org/uniprot/", "http://identifiers.org/ensembl.gene/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/wikipathways/", "http://identifiers.org/pubmed/"],
+    "output": ["http://identifiers.org/ncbigene/"],
     "jsonld": "context/mygene_query_context.json",
     "api": "mygene.info",
     "type": "query"
   },
   {
     "url_syntax": "http://mygene.info/v3/gene/{{input}}",
-    "input": ["entrez_gene_id"],
-    "output": ["uniprot_id", "ensembl_gene_id", "hgnc_gene_symbol", "wikipathway_id", "pubmed_id"],
+    "input": ["http://identifiers.org/ncbigene/"],
+    "output": ["http://identifiers.org/uniprot/", "http://identifiers.org/ensembl.gene/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/wikipathways/", "http://identifiers.org/pubmed/"],
     "jsonld": "context/mygene_context.json",
     "api": "mygene.info",
     "type": "get"
   },
   {
     "url_syntax": "http://myvariant.info/v1/query?q={{input}}&fetch_all=TRUE&fields=_id",
-    "input": ["entrez_gene_id", "hgnc_gene_symbol", "ensembl_gene_id", "dbsnp_id", "pubmed_id", "uniprot_id"],
-    "output": ["hgvs_id"],
+    "input": ["http://identifiers.org/ncbigene/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/ensembl.gene/", "http://identifiers.org/dbsnp/", "http://identifiers.org/pubmed/", "http://identifiers.org/uniprot/"],
+    "output": ["http://identifiers.org/hgvs/"],
     "jsonld": "context/myvariant_query_context.json",
     "api": "myvariant.info",
     "type": "query"
   },
   {
     "url_syntax": "http://myvariant.info/v1/variant/{{input}}",
-    "input": ["hgvs_id"],
-    "output": ["entrez_gene_id", "hgnc_gene_symbol", "ensembl_gene_id", "dbsnp_id", "pubmed_id", "uniprot_id"],
+    "input": ["http://identifiers.org/hgvs/"],
+    "output": ["http://identifiers.org/ncbigene/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/ensembl.gene/", "http://identifiers.org/dbsnp/", "http://identifiers.org/pubmed/", "http://identifiers.org/uniprot/"],
     "jsonld": "context/myvariant_context.json",
     "api": "myvariant.info",
     "type": "get"
   },
   {
     "url_syntax": "http://mychem.info/v1/query?q={{input}}&fetch_all=TRUE&fields=_id",
-    "input": ["dbsnp_id", "pubchem_id", "drugbank_id", "pubmed_id", "hgnc_gene_symbol", "uniprot_id", "clinicaltrial_id"],
-    "output": ["inchikey"],
+    "input": ["http://identifiers.org/dbsnp/", "http://identifiers.org/pubchem.compound/", "http://identifiers.org/drugbank/", "http://identifiers.org/pubmed/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/uniprot/", "http://identifiers.org/clinicaltrials/"],
+    "output": ["http://identifiers.org/inchikey/"],
     "jsonld": "context/mychem_query_context.json",
     "api": "mychem.info",
     "type": "query"
   },
   {
     "url_syntax": "http://mychem.info/v1/drug/{{input}}",
-    "input": ["inchikey", "drugbank_id"],
-    "output": ["dbsnp_id", "pubchem_id", "drugbank_id", "pubmed_id", "hgnc_gene_symbol", "uniprot_id", "clinicaltrial_id"],
+    "input": ["http://identifiers.org/inchikey/", "http://identifiers.org/drugbank/"],
+    "output": ["http://identifiers.org/dbsnp/", "http://identifiers.org/pubchem.compound/", "http://identifiers.org/drugbank/", "http://identifiers.org/pubmed/", "http://identifiers.org/hgnc.symbol/", "http://identifiers.org/uniprot/", "http://identifiers.org/clinicaltrials/"],
     "jsonld": "context/mychem_query_context.json",
     "api": "mychem.info",
     "type": "get"
